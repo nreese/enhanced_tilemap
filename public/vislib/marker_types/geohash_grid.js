e01e9d70e7493cd7439024ed0e929c8a0703a017
nreese <reese.nathan@gmail.com>
2016-09-12 09:37:04 -0600

    update to kibana4.6 versions

--- public/vislib/marker_types/geohash_grid.js
+++ public/vislib/marker_types/geohash_grid.js
@@ -1,39 +1,40 @@
-import _ from 'lodash';
-import L from 'leaflet';
-import VislibVisualizationsMarkerTypesBaseMarkerProvider from './base_marker';
-export default function GeohashGridMarkerFactory(Private) {
+define(function (require) {
+  return function GeohashGridMarkerFactory(Private) {
+    let _ = require('lodash');
+    let L = require('leaflet');
 
-  let BaseMarker = Private(VislibVisualizationsMarkerTypesBaseMarkerProvider);
+    let BaseMarker = Private(require('ui/vislib/visualizations/marker_types/base_marker'));
 
-  /**
-   * Map overlay: rectangles that show the geohash grid bounds
-   *
-   * @param map {Leaflet Object}
-   * @param geoJson {geoJson Object}
-   * @param params {Object}
-   */
-  _.class(GeohashGridMarker).inherits(BaseMarker);
-  function GeohashGridMarker(map, geoJson, params) {
-    let self = this;
-    GeohashGridMarker.Super.apply(this, arguments);
+    /**
+     * Map overlay: rectangles that show the geohash grid bounds
+     *
+     * @param map {Leaflet Object}
+     * @param geoJson {geoJson Object}
+     * @param params {Object}
+     */
+    _.class(GeohashGridMarker).inherits(BaseMarker);
+    function GeohashGridMarker(map, geoJson, params) {
+      let self = this;
+      GeohashGridMarker.Super.apply(this, arguments);
 
-    // super min and max from all chart data
-    let min = this.geoJson.properties.allmin;
-    let max = this.geoJson.properties.allmax;
+      // super min and max from all chart data
+      let min = this.geoJson.properties.allmin;
+      let max = this.geoJson.properties.allmax;
 
-    this._createMarkerGroup({
-      pointToLayer: function (feature, latlng) {
-        let geohashRect = feature.properties.rectangle;
-        // get bounds from northEast[3] and southWest[1]
-        // corners in geohash rectangle
-        let corners = [
-          [geohashRect[3][0], geohashRect[3][1]],
-          [geohashRect[1][0], geohashRect[1][1]]
-        ];
-        return L.rectangle(corners);
-      }
-    });
-  }
+      this._createMarkerGroup({
+        pointToLayer: function (feature, latlng) {
+          let geohashRect = feature.properties.rectangle;
+          // get bounds from northEast[3] and southWest[1]
+          // corners in geohash rectangle
+          let corners = [
+            [geohashRect[3][0], geohashRect[3][1]],
+            [geohashRect[1][0], geohashRect[1][1]]
+          ];
+          return L.rectangle(corners);
+        }
+      });
+    }
 
-  return GeohashGridMarker;
-};
+    return GeohashGridMarker;
+  };
+});
