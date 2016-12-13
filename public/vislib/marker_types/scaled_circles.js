6a884ab25401ae21615cf1b310398533a3495ca8
JacobBrandt <jbrandt@dcsdk12.org>
2016-12-13 14:12:12 -0700

    Fixed issue with overlapping scaled circle markers (#19)
    * Fixed issue with overlapping scaled circle markers
    
    * Avoid divide by zero
    
    * Use L.circleMarker instead of L.circle so that the radius is not rounded.
    
    * Refactor mistake

--- public/vislib/marker_types/scaled_circles.js
+++ public/vislib/marker_types/scaled_circles.js
@@ -17,43 +17,86 @@ define(function (require) {
       let self = this;
       ScaledCircleMarker.Super.apply(this, arguments);
 
-      // multiplier to reduce size of all circles
-      let scaleFactor = 0.6;
+      // Earth circumference in meters
+      const earthCircumference = 40075017;
+      const mapZoom = map.getZoom();
+      const latitudeRadians = map.getCenter().lat * (Math.PI/180);
+      this._metersPerPixel = earthCircumference * Math.cos(latitudeRadians) / Math.pow(2, mapZoom + 8);
 
       this._createMarkerGroup({
         pointToLayer: function (feature, latlng) {
-          let value = feature.properties.value;
-          let scaledRadius = self._radiusScale(value) * scaleFactor;
+          let scaledRadius = self._radiusScale(feature);
           return L.circleMarker(latlng).setRadius(scaledRadius);
         }
       });
     }
 
     /**
-     * radiusScale returns a number for scaled circle markers
-     * for relative sizing of markers
+     * _geohashMinDistance returns a min distance in meters for sizing
+     * circle markers to fit within geohash grid rectangle
      *
-     * @method _radiusScale
-     * @param value {Number}
+     * @method _geohashMinDistance
+     * @param feature {Object}
      * @return {Number}
      */
-    ScaledCircleMarker.prototype._radiusScale = function (value) {
-      let precisionBiasBase = 5;
-      let precisionBiasNumerator = 200;
-      let zoom = this.map.getZoom();
-      let maxValue = this.geoJson.properties.allmax;
-      let precision = _.max(this.geoJson.features.map(function (feature) {
-        return String(feature.properties.geohash).length;
-      }));
+    ScaledCircleMarker.prototype._geohashMinDistance = function (feature) {
+      let centerPoint = _.get(feature, 'properties.center');
+      let geohashRect = _.get(feature, 'properties.rectangle');
 
-      let pct = Math.abs(value) / Math.abs(maxValue);
-      let zoomRadius = 0.5 * Math.pow(2, zoom);
-      let precisionScale = precisionBiasNumerator / Math.pow(precisionBiasBase, precision);
+      // centerPoint is an array of [lat, lng]
+      // geohashRect is the 4 corners of the geoHash rectangle
+      //   an array that starts at the southwest corner and proceeds
+      //   clockwise, each value being an array of [lat, lng]
 
-      // square root value percentage
-      return Math.pow(pct, 0.5) * zoomRadius * precisionScale;
+      // center lat and southeast lng
+      let east   = L.latLng([centerPoint[0], geohashRect[2][1]]);
+      // southwest lat and center lng
+      let north  = L.latLng([geohashRect[3][0], centerPoint[1]]);
+
+      // get latLng of geohash center point
+      let center = L.latLng([centerPoint[0], centerPoint[1]]);
+
+      // get smallest radius at center of geohash grid rectangle
+      let eastRadius  = Math.floor(center.distanceTo(east));
+      let northRadius = Math.floor(center.distanceTo(north));
+      return _.min([eastRadius, northRadius]);
     };
 
+    /**
+     * _radiusScale returns the radius (in pixels) of the feature based on its
+     * value.  The radius fits within the geohash bounds of the feature to
+     * avoid overlapping.
+     *
+     * @method _scaleValueBetween
+     * @param feature {Object} - The feature
+     * @return {Number}
+     */
+    ScaledCircleMarker.prototype._radiusScale = function(feature) {
+      let radius = this._geohashMinDistance(feature);
+      let orgMin = this.geoJson.properties.allmin;
+      let orgMax = this.geoJson.properties.allmax;
+      // Don't let the circle size get any smaller than one-third the max size
+      let min = orgMax / 3;
+      let max = orgMax;
+      let value = this._scaleValueBetween(feature.properties.value, min, max, orgMin, orgMax);
+      return radius * (value / max) / this._metersPerPixel;
+    }
+
+    /**
+     * _scaleValueBewteen returns the given value between the new min and max based
+     * on the original scale
+     *
+     * @method _scaleValueBetween
+     * @param value {Number} - The value to scale
+     * @param min {Number} - The new minimum
+     * @param max {Number} - The new maximum
+     * @param orgMin {Number} - The original minimum
+     * @param feature {Number} - The original maximum
+     * @return {Number}
+     */
+    ScaledCircleMarker.prototype._scaleValueBetween = function(value, min, max, orgMin, orgMax) {
+      return (orgMin != orgMax) ? ((max-min)*(value-orgMin))/(orgMax-orgMin) + min : value;
+    }
 
     return ScaledCircleMarker;
   };
