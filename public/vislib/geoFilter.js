5c1e26005309e2a3b8a449a4d9004c34860984b3
Jacob Brandt <krim.2206@gmail.com>
2017-11-20 14:52:28 -0700

    Allow circle as drawable control which applies a geo_distance filter when created

--- public/vislib/geoFilter.js
+++ public/vislib/geoFilter.js
@@ -249,11 +249,34 @@ define(function (require) {
       return geofilter;
     }
 
+    /**
+     * Create elasticsearch geospatial geo_distance filter
+     *
+     * @method circleFilter
+     * @param fieldname {String} name of geospatial field in IndexPattern
+     * @param lat {Object} latitude of center point for circle (decimal degrees)
+     * @param lon {Object} longitude of center point for circle (decimal degrees)
+     * @param radius {Object} radius
+     * @return {Object} elasticsearch geospatial geo_distance filter
+     */
+    function circleFilter(fieldname, lat, lon, radius) {
+      let geofilter = null;
+      geofilter = { geo_distance: {
+        distance: radius
+      }};
+      geofilter.geo_distance[fieldname] = {
+        lat: lat,
+        lon: lon
+      };
+      return geofilter;
+    }
+
     return {
       add: addGeoFilter,
       isGeoFilter: isGeoFilter,
       getGeoFilters: getGeoFilters,
-      rectFilter: rectFilter
+      rectFilter: rectFilter,
+      circleFilter: circleFilter
     }
   }
 });
