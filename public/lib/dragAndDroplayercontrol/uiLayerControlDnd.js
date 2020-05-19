42f8196ab27e50e9f8b29dfe5a0a765b6b540b46
corrigancd <edwintcorrigan@gmail.com>
2020-05-19 21:10:55 +0100

    Drag handles changed from vertical to horizontal

--- public/lib/dragAndDroplayercontrol/uiLayerControlDnd.js
+++ public/lib/dragAndDroplayercontrol/uiLayerControlDnd.js
@@ -134,7 +134,7 @@ export class LayerControlDnd extends React.Component {
                         )}
                       >
                         <span {...provided.dragHandleProps} className="panel-drag-handler">
-                          <span className={'fas fa-grip-horizontal'}
+                          <span className={'fas fa-grip-vertical'}
                           />
                         </span>
 
